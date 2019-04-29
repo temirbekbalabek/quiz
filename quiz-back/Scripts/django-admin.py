544b0fb5c0cf9e33028bc18e83fc8eae2132a761
temirbekbalabek <temirbek.balabek@mail.ru>
2019-04-30 02:53:02 +0600

    test

---
+++ quiz-back/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\temir\onedrive\рабочий стол\subjects\quiz\quiz-back\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
